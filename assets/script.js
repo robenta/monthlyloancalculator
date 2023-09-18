55cc19924999156a368534c86d6e948d707293a1
robenta <robentaahimie4@gmail.com>
2023-09-18 06:17:02 +0100

    first commit

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+const calculate = document.getElementById('calculate').addEventListener('click', computeLoan)
+function computeLoan(){
+    const amount = document.getElementById('amount').value;
+    const interest_rate = document.getElementById('interest_rate').value;
+    const months = document.getElementById('months').value;
+    const interest = (amount * (interest_rate * .01)) / months;
+    var payment = ((amount / months) + interest).toFixed(2);
+    payment = payment.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    document.getElementById('payment').innerHTML = `Monthly Payment = $${payment}`;
+}
